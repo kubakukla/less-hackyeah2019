1a9c68ae5d4286c0346f20ee8b2853d806029b61
Krzysztof Foszcz <krzysztof.foszcz@gmail.com>
2019-09-15 00:44:44 +0200

    add order fixtures

--- api2/src/DataFixtures/AppFixtures.php
+++ api2/src/DataFixtures/AppFixtures.php
@@ -2,8 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Order;
+use App\Entity\OrderItem;
 use App\Entity\Product;
 use App\Entity\User;
+use App\Repository\ProductRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -11,11 +14,21 @@ class AppFixtures extends Fixture
 {
     const NUMBER_OF_PRODUCTS = 10;
 
+    /** @var ProductRepository */
+    private $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
     public function load(ObjectManager $manager)
     {
         $this->loadProducts($manager);
         $this->loadUsers($manager);
+        $manager->flush();
 
+        $this->loadOrders($manager);
         $manager->flush();
     }
 
@@ -42,5 +55,29 @@ class AppFixtures extends Fixture
         $user->setName('Krzysiek');
         $user->setMail('krzysiek@bluepaprica.com');
         $manager->persist($user);
+    }
+
+    private function loadOrders(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $order = new Order();
+            $order->setUserId(1);
+            $hoursAgo = random_int(-4, -1) - $i;
+            $order->setCreatedAt(new \DateTime($hoursAgo.' hours'));
+            $manager->persist($order);
+
+            $productCount = random_int(2, 5);
+            for ($j = 0; $j < $productCount; $j++) {
+                $item = new OrderItem();
+
+                $productId = random_int(1, self::NUMBER_OF_PRODUCTS);
+                $product = $this->productRepository->find($productId);
+
+                $item->setProduct($product);
+                $item->setQuantity(random_int(1, 3));
+                $item->setOrder($order);
+                $manager->persist($item);
+            }
+        }
     }
 }
