220f731acaebb282feb9d82cca086870ac15b7c1
Krzysztof Foszcz <krzysztof.foszcz@gmail.com>
2019-09-14 18:55:09 +0200

    add product fixtures

--- api2/src/DataFixtures/AppFixtures.php
+++ api2/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Products;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    const NUMBER_OF_PRODUCTS = 10;
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadProducts($manager);
 
         $manager->flush();
+    }
+
+    private function loadProducts(ObjectManager $manager)
+    {
+        for ($i = 1; $i <= self::NUMBER_OF_PRODUCTS; $i++) {
+            $product = new Products();
+            $product->setName("Product ${i}");
+            $product->setType($i % 5 + 1);
+            $product->setCode($i);
+            $manager->persist($product);
+        }
     }
 }
