11a531d700ac5d12047ff3a49a91dd991b1184fe
Robert Szczepaniec <robert.szczepaniec@bluepaprica.com>
2019-09-15 03:37:51 +0200

    Connected home page view with api

--- front/less/src/components/LastChoices/LastChoice.vue
+++ front/less/src/components/LastChoices/LastChoice.vue
@@ -1,14 +1,15 @@
 <template>
   <router-link to="/details" tag="article" class="last-buy">
     <div class="last-buy__header">
-      <img alt="Vue logo" src="./../../assets/img/logos/spar.png"><em>|</em> 9 items
-      <span class="date">one day ago</span>
+      <img alt="Vue logo" src="./../../assets/img/logos/spar.png"><em>|</em> Number of items: {{count}}
+      <span class="date">{{date}}</span>
     </div>
     <div class="sort-list">
-      <span class="plastic">Plastic</span>
-      <span class="paper">Paper</span>
-      <span class="general">General</span>
-      <span class="bio">Bio</span>
+      <span class="plastic" v-if="garbage.plastic">Plastic</span>
+      <span class="paper" v-if="garbage.paper">Paper</span>
+      <span class="general" v-if="garbage.general">General</span>
+      <span class="bio" v-if="garbage.bio">Bio</span>
+      <span class="glass" v-if="garbage.glass">Glass</span>
     </div>
   </router-link>
 </template>
@@ -17,6 +18,7 @@
 
 
   export default {
-    name: 'LastChoice'
+    name: 'LastChoice',
+    props: ['count', 'garbage', 'date', 'order_id']
   }
 </script>
--- front/less/src/components/LastChoices/LastChoices.vue
+++ front/less/src/components/LastChoices/LastChoices.vue
@@ -4,15 +4,12 @@
       <div class="container container--inside">
         <h2>Latest choices</h2>
       </div>
-      <LastChoice />
-      <LastChoice />
-      <LastChoice />
-      <LastChoice />
-      <LastChoice />
-      <LastChoice />
-      <LastChoice />
-      <LastChoice />
-      <LastChoice />
+      <LastChoice v-for="lastChoice in lastChoices.data"
+                  v-bind:count="lastChoice.item_count"
+                  v-bind:garbage="lastChoice.totals"
+                  v-bind:date="lastChoice.created_at | formatDate"
+                  v-bind:order_id="lastChoice.id"
+      />
     </div>
   </div>
 </template>
@@ -25,6 +22,30 @@
     name: 'LastChoices',
     components: {
       LastChoice
-    }
+    },
+    data: function () {
+      return {
+        lastChoices: []
+      }
+    },
+    mounted () {
+      this.axios.get('http://api.hackyeah.bluepaprica.ovh/user/1/orders').then((response) => {
+        this.lastChoices = response;
+      }).catch(error => {
+        console.log(error);
+      })
+    },
+    filters: {
+      formatDate: function (value) {
+        if (!value) {return ''}
+        let data = new Date(value * 1000);
+        let day = data.getUTCDay();
+        day = day < 10 ? '0' + day : day;
+        let month = data.getMonth();
+        month = month < 10 ? '0' + month : month;
+        let year = data.getFullYear();
+        return day + "/" + month + "/" + year;
+      }
+    },
   }
 </script>
